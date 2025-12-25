c0e72ef7d5d625fb5afe0654bad976d356d2ea6a
paulodevelopment20@gmail.com <paulodevelopment20@gmail.com>
2025-12-25 21:57:23 +0800

    Add base framework

---
+++ MiniSteam/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MiniSteam.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ MiniSteam/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniSteam.Domain.Entities;
+
+namespace MiniSteam.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Game> Games { get; set; }
+    }
+}
